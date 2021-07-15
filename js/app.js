a063ddc9563c63421d309e3b790110a567a68454
bonino97 <juanbonino97@gmail.com>
2021-07-14 23:28:39 -0300

    Fixing name convention

--- js/app.js
+++ js/app.js
@@ -1,136 +1,124 @@
-// 5242ceab92b609d484d547117508a971 -> Api key.
-const container = document.querySelector('.container');
-const resultado = document.getElementById('resultado');
-const formulario = document.getElementById('formulario');
+const apiKey = '942efe42fad7cec9560614b927352b66';
+const alertContainer = document.getElementById('alertContainer');
 
-window.addEventListener('load', () => {
-  formulario.addEventListener('submit', buscarClima);
-});
-
-function buscarClima(e) {
+function getWeather(e) {
   e.preventDefault();
-  const ciudad = document.getElementById('ciudad').value;
-  const pais = document.getElementById('pais').value;
-
-  if (ciudad === '' || pais === '') {
-    mostrarError('Ambos campos son obligatorios.');
-    return;
-  }
-
-  consultarAPI(ciudad, pais);
+  const city = document.getElementById('city').value;
+  apiPerDay(city);
 }
 
-function mostrarError(mensaje) {
-  const alerta = document.querySelector('.bg-red-100');
+function setDailyData(data) {
+  document.getElementById('actualTemp').innerHTML = `${kelvinToCent(
+    data.main.temp
+  )}°`;
+  document.getElementById('selectedCity').innerHTML = data.name;
+  document.getElementById('weatherDescription').innerHTML =
+    data.weather[0].description;
+  document.getElementById('weatherMain').innerHTML = data.weather[0].main;
+  document.getElementById('formattedDate').innerHTML = getDate(
+    data.dt,
+    data.timezone
+  );
+  document.getElementById('visibility').innerHTML = `${
+    data.visibility / 1000
+  } km`;
+  document.getElementById('humidity').innerHTML = `${data.main.humidity} %`;
+}
 
-  if (!alerta) {
-    const alerta = document.createElement('div');
-    alerta.classList.add(
-      'bg-red-100',
-      'border-red-400',
-      'text-red-700',
-      'px-4',
-      'py-3',
-      'rounded',
-      'max-w-md',
-      'mx-auto',
-      'mt-6',
-      'text-center'
-    );
-    alerta.innerHTML = `
-        <strong class="font-bold">Error!</strong>
-        <span class="block">${mensaje}</span>
-        `;
-    container.appendChild(alerta);
+function apiPerDay(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      clearHTML();
+      if (data.cod === '404') {
+        showError('City not found!');
+        return;
+      }
+      apiPerWeek(data.coord.lat, data.coord.lon);
+      setDailyData(data);
+    });
+}
+
+function apiPerWeek(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.daily);
+    });
+}
+
+function clearHTML() {
+  return;
+}
+
+function showError(msg) {
+  const alert = document.querySelector('.bg-red-100');
+
+  if (!alert) {
+    const alert = document.createElement('div');
+    alert.classList.add('error-message');
+    alert.innerHTML = `
+          <span class="block">${msg}</span>
+          `;
+    alertContainer.appendChild(alert);
 
     setTimeout(() => {
-      alerta.remove();
+      alert.remove();
     }, 3000);
   }
 }
 
-function consultarAPI(ciudad, pais) {
-  const appID = '942efe42fad7cec9560614b927352b66';
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appID}`;
-
-  spinner();
-
-  fetch(url)
-    .then((respuesta) => respuesta.json())
-    .then((datos) => {
-      limpiarHTML();
-
-      if (datos.cod === '404') {
-        mostrarError('Ciudad no encontrada');
-        return;
-      }
-
-      mostrarClima(datos);
-    });
+function kelvinToCent(grade) {
+  return parseInt(grade - 273.15);
 }
 
-function mostrarClima(datos) {
-  const {
-    name,
-    main: { temp, temp_max, temp_min },
-  } = datos;
-  const centigrados = kelvinACentigrados(temp);
-  const max = kelvinACentigrados(temp_max);
-  const min = kelvinACentigrados(temp_min);
+function getDate(dateTimeStamp) {
+  const dayNames = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
 
-  const nombreCiudad = document.createElement('p');
-  nombreCiudad.textContent = `Clima en ${name}`;
-  nombreCiudad.classList.add('font-bold', 'text-2xl');
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
 
-  const actual = document.createElement('p');
-  actual.innerHTML = `${centigrados} &#8451;`;
-  actual.classList.add('font-bold', 'text-6xl');
+  let date = new Date(dateTimeStamp * 1000);
+  let day = dayNames[date.getDay()];
+  let dayNumber = date.getDate();
+  let month = monthNames[date.getMonth()];
+  let hours = date.getHours();
+  let minutes = '0' + date.getMinutes();
+  let formattedDate =
+    day +
+    ' ' +
+    dayNumber +
+    ', ' +
+    month +
+    ', ' +
+    hours +
+    ':' +
+    minutes.substr(-2);
 
-  const tempMax = document.createElement('p');
-  tempMax.innerHTML = `Max: ${max} &#8451;`;
-  tempMax.classList.add('text-xl');
-
-  const tempMin = document.createElement('p');
-  tempMin.innerHTML = `Min: ${min} &#8451;`;
-  tempMin.classList.add('text-xl');
-
-  const resultadoDiv = document.createElement('div');
-  resultadoDiv.classList.add('text-center', 'text-white');
-
-  resultadoDiv.appendChild(nombreCiudad);
-  resultadoDiv.appendChild(actual);
-  resultadoDiv.appendChild(tempMax);
-  resultadoDiv.appendChild(tempMin);
-  resultado.appendChild(resultadoDiv);
-  console.log(centigrados);
-}
-
-const kelvinACentigrados = (grados) => parseInt(grados - 273.15);
-
-function limpiarHTML() {
-  while (resultado.firstChild) {
-    resultado.removeChild(resultado.firstChild);
-  }
-}
-
-function spinner() {
-  limpiarHTML();
-  const divSpinner = document.createElement('div');
-  divSpinner.classList.add('sk-fading-circle');
-  divSpinner.innerHTML = `
-    <div class="sk-circle1 sk-circle"></div>
-    <div class="sk-circle2 sk-circle"></div>
-    <div class="sk-circle3 sk-circle"></div>
-    <div class="sk-circle4 sk-circle"></div>
-    <div class="sk-circle5 sk-circle"></div>
-    <div class="sk-circle6 sk-circle"></div>
-    <div class="sk-circle7 sk-circle"></div>
-    <div class="sk-circle8 sk-circle"></div>
-    <div class="sk-circle9 sk-circle"></div>
-    <div class="sk-circle10 sk-circle"></div>
-    <div class="sk-circle11 sk-circle"></div>
-    <div class="sk-circle12 sk-circle"></div>
-    `;
-
-  resultado.appendChild(divSpinner);
+  return formattedDate;
 }
